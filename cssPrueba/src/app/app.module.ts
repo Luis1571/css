8ef087d04eb7a297b3d11ac220823f480f86db87
Luis1571 <55500525+Luis1571@users.noreply.github.com>
2022-05-23 21:31:14 +0200

    comimt 2

--- cssPrueba/src/app/app.module.ts
+++ cssPrueba/src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FormTransparenteComponent } from "./diseños/FormTransparente/FormTransparente.component";
+//import { FormTransparente } from "../app/diseños/FormTransparente/FormTransparente.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, FormTransparenteComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ cssPrueba/src/app/diseños/FormTransparente/FormTransparente.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "FormTransparente",
+  templateUrl: "./FormTransparente.component.html",
+  styleUrls: ["./FormTransparente.component.css"],
+})
+export class FormTransparenteComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
